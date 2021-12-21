49f1f6f66fe575252fd99fb27eb82288105fd852
James Ng <jamesng135@gmail.com>
2021-12-21 00:03:26 -0500

    added final version of chalkboard

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
-const app = express();
-const mysql = require("mysql");
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
